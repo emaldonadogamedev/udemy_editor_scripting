f795656f0f173c43da70e86868f62d4d625bb650
emaldonadogamedev <em_gamedev@outlook.com>
2025-01-11 14:56:55 -0400

    finished the course with the aspect ratio tester!

--- Assets/Example9/Editor/AspectRatioTester.cs
+++ Assets/Example9/Editor/AspectRatioTester.cs
@@ -1,16 +1,94 @@
 using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Reflection;
 
-public class AspectRatioTester : MonoBehaviour
-{
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+[InitializeOnLoad]
+public class AspectRatioTester : Editor
+{ 
+    static string applicationDataPath = Application.dataPath + "/";
+    static int numberOfScreenshotsSaved = 0;
+    static bool menuOptionWasClicked = false;
+    static bool startedScreenshot = false;
+    static readonly List<string> aspectRatios = new()
     {
-        
+        applicationDataPath + "Free Aspect.png",
+        applicationDataPath + "16:9 Aspect.png",
+        applicationDataPath + "16:10 Aspect.png",
+        applicationDataPath + "Full HD (1920 x 1080).png"
+    };
+
+    static AspectRatioTester()
+    {
+        EditorApplication.update += EditorToolLoop;
     }
 
-    // Update is called once per frame
-    void Update()
+    [MenuItem("Custom Tools/Test Aspect Ratios")]
+    static void TestAspectRatios()
     {
-        
+        menuOptionWasClicked = true;
+        startedScreenshot = false;
+        numberOfScreenshotsSaved = 0;
+    }
+
+    static void EditorToolLoop()
+    {
+        if (menuOptionWasClicked && startedScreenshot)
+        {
+            startedScreenshot = true;
+        }
+
+
+        if (menuOptionWasClicked && !startedScreenshot)
+        {
+            startedScreenshot = true;
+            SaveScreenshotAtAspectRatio(
+                numberOfScreenshotsSaved,
+                aspectRatios[numberOfScreenshotsSaved]);
+        }
+
+        if (numberOfScreenshotsSaved < aspectRatios.Count && System.IO.File.Exists(aspectRatios[numberOfScreenshotsSaved]))
+        {
+            numberOfScreenshotsSaved++;
+            startedScreenshot = false;
+
+            Refresh();
+        }
+            
+        if (numberOfScreenshotsSaved == aspectRatios.Count)
+        {
+            menuOptionWasClicked = false;
+        }
+    }
+
+    static void Refresh()
+    {
+        AssetDatabase.Refresh();
+    }
+
+    public static void SetSize(int index)
+    {
+        var gameViewWindowType =
+            typeof(Editor).Assembly.GetType("UnityEditor.GameView");
+
+        var gameViewWindow = EditorWindow.GetWindow(gameViewWindowType);
+
+        var SizeSelectionCallback =
+            gameViewWindowType.GetMethod(
+                "SizeSelectionCallback",
+                BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic);
+
+        SizeSelectionCallback.Invoke(gameViewWindow, new object[] { index, null });
+    }
+
+    public static void TakeScreenshot(string fileName)
+    {
+        ScreenCapture.CaptureScreenshot(fileName);
+    }
+
+    public static void SaveScreenshotAtAspectRatio(int index, string fileName)
+    {
+        SetSize(index);
+        TakeScreenshot(fileName);
     }
 }
